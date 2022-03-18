fa053ed3a30aef58368f99112a9dd13bd1b860e0
Xoel Mato Blanco <xoelmb@gmail.com>
2022-03-18 18:37:14 +0100

    run on cluster and fixes

--- create_env.sh
+++ create_env.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-conda create -n denovobs -y -c r -c conda -c anaconda -c conda-forge -c bioconda r-dplyr bioconductor-edger r-reshape2 r-ggplot2 r-reshape r-nlme r-readr r-data.table r-ggally bioconductor-complexheatmap r-rcolorbrewer r-irkernel bedtools
+conda create -n denovobs -y -c r -c conda -c anaconda -c conda-forge -c bioconda 'r-base>=4' r-dplyr bioconductor-edger r-reshape2 'r-ggplot2>= 3.3.0' r-reshape r-nlme r-readr r-data.table r-ggally bioconductor-complexheatmap r-rcolorbrewer r-irkernel bedtools
+
+mamba create -p ~/.conda/envs/denovobs -y -c r -c conda -c anaconda -c conda-forge -c bioconda 'r-base>=4' r-dplyr bioconductor-edger r-reshape2 'r-ggplot2>= 3.3.0' r-reshape r-nlme r-readr r-data.table r-ggally bioconductor-complexheatmap r-rcolorbrewer r-irkernel bedtools
